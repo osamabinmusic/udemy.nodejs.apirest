214252eeabbd7743f24132a0da3ade5729020762
Victor Nilo <osamabinmusic@hotmail.com>
2020-06-05 11:42:40 -0400

    - Se agrega validacion - Conexion a MongoDB - Manejo basico de errores - Implementacion inicial de manejo de rutas estaticas para  almacenamiento de imagenes.

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static('/images', express.static(path.join(__dirname, 'images'))));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use( (error, req, res, next) => {
+	console.log( error );
+	const status = error.statusCode;
+	const message = error.message;
+	res.status(status).json({ message: message });
+})
+
+mongoose
+	.connect(
+		`mongodb+srv://osamabinmusic:Conchetumadre.,1595@vinilo-cluster-test001-e9dtn.gcp.mongodb.net/messages?retryWrites=true`,
+		{ useNewUrlParser: true }
+	)
+	.then( result => {
+		app.listen(8080);
+	})
+	.catch( err => console.log('Error mongoose', err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,53 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {title: 'First Post', content: 'This is a post'}
-        ]
-    });
+	res.status(200).json({
+		posts: [
+			{
+				_id: 1,
+				title: 'First Post',
+				content: 'This is a post',
+				imageUrl: 'images/duck.jpg',
+				creator: {
+					name: 'Victor Nilo',
+				},
+				createdAt: new Date(),
+			}
+		]
+	});
 };
 
 exports.createPost = (req, res, next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    res.status(201).json( {
-        message: 'Post created successfully',
-        post: { id: new Date().toISOString(), title: title, content: content }
-    });
+	const errors = validationResult(req);
+	if(!errors.isEmpty()) {
+		const error = new Error('Validation failed, entered data is incorrect.');
+		error.statusCode = 422;
+		throw error;
+	}
+
+	const title = req.body.title;
+	const content = req.body.content;
+	const post = new Post({
+		title: title,
+		content: content,
+		imageUrl: 'images/duck.jpg',
+		creator: { name: 'Victor Nilo' },
+	});
+
+	post
+		.save()
+		.then(result => {
+			res.status(201).json( {
+				message: 'Post created successfully',
+				post: result,
+			});
+		})
+		.catch( err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
 };
