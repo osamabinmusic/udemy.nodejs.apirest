23f64bed2cae501f754498e88a0bfd08a8f9fb71
Victor Nilo <osamabinmusic@hotmail.com>
2020-06-05 12:55:34 -0400

    - Muestra Mensaje con imagen

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/images', express.static('/images', express.static(path.join(__dirname, 'images'))));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
