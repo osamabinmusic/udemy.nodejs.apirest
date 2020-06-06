813db47bc62e5a5b85f1b88ed8f8152f66ed4f5d
Victor Nilo <osamabinmusic@hotmail.com>
2020-06-05 21:11:49 -0400

    Actualizar y eliminar posts - Validacion de image si es distinta - Uso de multer

--- app.js
+++ app.js
@@ -3,17 +3,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString() + '-' + file.originalname);
+	}
+});
+
+const fileFilter = (req, file, cb) => {
+	if ( ['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype) ) {
+		cb(null, true)
+	} else {
+		cb(null, false);
+	}
+}
+
 app.use(bodyParser.json());
+app.use(
+	multer({storage: fileStorage, fileFilter: fileFilter}).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	next();
 });
