f4b4621c8cb5c30b5dea37789e1253e620534551
Victor Nilo <osamabinmusic@hotmail.com>
2020-06-07 17:13:20 -0400

    Pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,12 +5,29 @@ const path = require('path');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+	const currentPage = req.query.page || 1;
+	const perPage = 2;
+	let totalItems;
 	Post
 		.find()
+		.countDocuments()
+		.then(total => {
+			totalItems = total;
+			return Post
+				.find()
+				.skip((currentPage -1) * perPage)
+				.limit(perPage)
+				;
+		})
 		.then(posts => {
 			res
 				.status(200)
-				.json({ message: 'Fetched posts successfully.', posts: posts });
+				.json({
+					message: 'Fetched posts successfully.',
+					posts: posts,
+					totalItems: totalItems
+				})
+				;
 		})
 		.catch( err => {
 			if (!err.statusCode) {
